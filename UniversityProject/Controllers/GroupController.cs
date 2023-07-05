e9d077dfe35ec14e0e390ea9a350334969810d49
AdilTehranli <adiljt@code.edu.az>
2023-07-05 19:45:03 +0400

    Created UniversityProject

---
+++ UniversityProject/Controllers/GroupController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityProject.Models;
+
+namespace UniversityProject.Controllers
+{
+    public class GroupController : Controller
+    {
+        private List<Groups> _groups;
+        public GroupController()
+        {
+            _groups = new List<Groups> { new Groups { Id = 1, Name = "p110" }, new Groups { Id = 2, Name = "p112" }, new Groups { Id = 3, Name = "p115" } };
+        }
+        public IActionResult Index()
+        {
+            return View(_groups);
+        }
+    }
+}
---
+++ UniversityProject/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityProject.Models;
+
+namespace UniversityProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private List<Student> _students;
+        public StudentController()
+        {
+            _students = new List<Student>
+            {
+                new Student{Id=1,Name="Kamil",Surname="Zeynalli",Group="p110"},
+                new Student{Id=2,Name="Agakisi",Surname="Neymetulla",Group="p112"},
+                new Student{Id=3,Name="Nigar",Surname="Memmedli",Group="p115"},
+                new Student{Id=4,Name="Rovsen",Surname="Eliyev",Group="p118"}
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(_students);
+        }
+        public IActionResult Detail(int id)
+        {
+            var student = _students.Find(x => x.Id == id);
+            return View(student);
+        }
+    }
+}
